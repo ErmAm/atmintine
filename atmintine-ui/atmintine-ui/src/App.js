4c69e8ee099cc7ba5545685b0550f2be9234646a
ErmAm <erminas.ambrasas@gmail.com>
2021-06-10 14:24:49 +0300

    Pridėta biški JS failų , kol kas dar griaučius gaminu.

--- atmintine-ui/atmintine-ui/src/App.js
+++ atmintine-ui/atmintine-ui/src/App.js
@@ -1,16 +1,41 @@
 
 import TestComponent from "./components/TestComponent";
+import BasicTable from "./components/InfoTable/InfoTable";
+import ActionButtonClicked from "./components/TestingComponents/Button/Button_t_1";
+import Header from "./components/Header/Header";
+import Content from "./components/Content/Content";
+import Footer from "./components/Footer/Footer";
+import {CssBaseline} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+    }
+}));
+
 
 function App() {
+    const classes = useStyles()
 
   return (
-    <div className="App">
-        <h1>Holla amygos</h1>
-        <TestComponent user={{
-            name: "Kitoks",
-            surame: "Pvadinimas"
+    <div className={classes.root}>
+        {/*<h1>Holla amygos</h1>*/}
+        {/*<TestComponent user={{*/}
+        {/*    name: "Kitoks",*/}
+        {/*    surame: "Pvadinimas"*/}
 
-        }} />
+        {/*}} />*/}
+
+        {/*<BasicTable/>*/}
+        {/*<ActionButtonClicked/>*/}
+        <CssBaseline />
+        <Header/>
+        <Content/>
+
+        <Footer/>
     </div>
   );
 }
---
+++ atmintine-ui/atmintine-ui/src/components/Header/Header.js
@@ -0,0 +1,49 @@
+import AppBar from "@material-ui/core/AppBar";
+import {Toolbar} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import Button from "@material-ui/core/Button";
+import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+        borderBottom: `1px solid ${theme.palette.divider}`,
+    },
+    toolbarTitle: {
+        flexGrow: 1,
+    },
+    link: {
+        margin: theme.spacing(1, 1.5),
+    },
+    toolbar: {
+        flexWrap: 'wrap',
+    }
+}))
+
+export default () => {
+    const classes = useStyles()
+    return (
+        <>
+            <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
+                <Toolbar className={classes.toolbar}>
+                    <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+                       Home
+                    </Typography>
+                    <nav>
+                        <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+                            Mem Library
+                        </Link>
+                        <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+
+                        </Link>
+                    </nav>
+                    <Button href="#" color="primary" variant="outlined" className={classes.link}>
+                        Login
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+

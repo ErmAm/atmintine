d00663ea040573f83ee34b25a4547d19da11da6f
ErmAm <erminas.ambrasas@gmail.com>
2021-06-11 01:33:15 +0300

    tikrinu ar nesusigadino branchas

--- atmintinev1/src/main/resources/database/initial-data.sql
+++ atmintinev1/src/main/resources/database/initial-data.sql
@@ -9,5 +9,6 @@ VALUES
 INSERT INTO tagitem(id, name,latitude,longitude,description,real_place_name,has_shed,has_fireplace,has_wc,has_lake_nearby)
 VALUES
 ('00000000-f000-000a-000f-000000000001','Pušinelis','52.44423','24.56444','Viskas labai gerai','Trakai',true,true,true,true),
-('00000000-f000-000a-000f-000000000002','Vaivadiškės','52.41423','24.54444','Dalykai','Semeniškės',true,false,false,true)
+('00000000-f000-000a-000f-000000000002','Vaivadiškės','52.41423','24.54444','Dalykai','Semeniškės',true,false,false,true),
+('00000000-f000-000a-000f-000000000002','Kempinkiemis','52.49423','24.53444','Dalykai','Semeniškės',true,false,false,true)
 ;

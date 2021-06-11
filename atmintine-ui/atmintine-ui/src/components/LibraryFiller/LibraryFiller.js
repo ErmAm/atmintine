94779381f416401608c60bd595c450d7bb1b7ba2
ErmAm <erminas.ambrasas@gmail.com>
2021-06-11 13:47:35 +0300

    Pridėjau lentą kuri rodo išsaugotus tagus.

--- atmintine-ui/atmintine-ui/src/components/LibraryFiller/LibraryFiller.js
+++ atmintine-ui/atmintine-ui/src/components/LibraryFiller/LibraryFiller.js
@@ -1,18 +1,81 @@
 import {fetchGpsTags} from '../../api/GpsTagAPI'
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import {makeStyles} from "@material-ui/core/styles";
+import {Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
+import Container from "@material-ui/core/Container";
+import table from "table";
+import {render} from "@testing-library/react";
+
+
+// 1.1 susikuriam stiliuko f-ja
+const useStyle = makeStyles( {
+    table:{
+        maxWidth: 650,
+
+    }
+})
+
 
 const LibraryFiller = () => {
 
+    const [gpsTagList, setGpsTagList] = useState([])
+
     useEffect(()=>{
-        fetchGpsTags().then(response => console.log("response", response))
-            .catch(error => console.log("error",error))
-        }
+        fetchGpsTags()
+            .then(({data}) =>
+                setGpsTagList(data))
+            // .catch(error => console.log("error",error))
+        },[]
     )
 
 
+    // 1.1 Išsikviečiam stiliuko f-ja.
+    const clasess = useStyle()
+
     return(
         <>
-            <h1>I am librarry filler component</h1>
+            {/*<h1>I am librarry filler component</h1>*/}
+            {/*<pre>*/}
+            {/*    {JSON.stringify(gpsTagList, null,2)}*/}
+            {/*</pre>*/}
+            <Container maxWidth="md">
+                <TableContainer component={Paper} className={table}>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Location name</TableCell>
+                                <TableCell align="right">Latitude</TableCell>
+                                <TableCell align="right">Longitude</TableCell>
+                                <TableCell align="right">RealPlaceName</TableCell>
+                                <TableCell align="right">Description</TableCell>
+                                <TableCell align="right">Has Shed</TableCell>
+                                <TableCell align="right">Has Fireplace</TableCell>
+                                <TableCell align="right">Has Lake Nearby</TableCell>
+                                <TableCell align="right">Has WC</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {gpsTagList.map(gpsTag => (
+                                <TableRow key={gpsTag.id}>
+                                    <TableCell component="th" scope="row">
+                                        {gpsTag.name}
+                                    </TableCell>
+                                    <TableCell align="right">{gpsTag.latitude}</TableCell>
+                                    <TableCell align="right">{gpsTag.longitude}</TableCell>
+                                    <TableCell align="right">{gpsTag.realPlaceName}</TableCell>
+                                    <TableCell align="right">{gpsTag.description}</TableCell>
+                                    <TableCell align="right">{(gpsTag.hasShed ? "True" : "False")}</TableCell>
+                                    <TableCell align="right">{(gpsTag.hasFireplace ? "True" : "False")}</TableCell>
+                                    <TableCell align="right">{(gpsTag.hasLakeNearby ? "True" : "False")}</TableCell>
+                                    <TableCell align="right">{(gpsTag.hasWC ? "True" : "False")}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Container>
+
+
 
         </>
     )
--- atmintinev1/src/main/resources/database/initial-data.sql
+++ atmintinev1/src/main/resources/database/initial-data.sql
@@ -10,5 +10,5 @@ INSERT INTO tagitem(id, name,latitude,longitude,description,real_place_name,has_
 VALUES
 ('00000000-f000-000a-000f-000000000001','Pušinelis','52.44423','24.56444','Viskas labai gerai','Trakai',true,true,true,true),
 ('00000000-f000-000a-000f-000000000002','Vaivadiškės','52.41423','24.54444','Dalykai','Semeniškės',true,false,false,true),
-('00000000-f000-000a-000f-000000000002','Kempinkiemis','52.49423','24.53444','Dalykai','Semeniškės',true,false,false,true)
+('00000000-f000-000a-000f-000000000003','Kempinkiemis','52.49423','24.53444','Dalykai','Semeniškės',true,false,false,true)
 ;

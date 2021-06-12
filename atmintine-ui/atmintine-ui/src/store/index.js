6fdbe22c519a39e6e6a0cfee51e7dedeade49ed9
ErmAm <erminas.ambrasas@gmail.com>
2021-06-12 20:27:53 +0300

    Reduxo toolkitas paleistas.

--- atmintine-ui/atmintine-ui/src/store/index.js
+++ atmintine-ui/atmintine-ui/src/store/index.js
@@ -1,24 +1,35 @@
-import tagItem from "./slices/tagItemSlice";
+import tagItemReducer from "./slices/tagItemSlice";
 import {combineReducers, compose, createStore} from "redux";
+import {configureStore} from "@reduxjs/toolkit";
+import {logger} from "redux-logger/src";
 
 
 
 // root reduceris yra funckija guanama iškvietus kitą f-ją.
-const rootReducer = combineReducers({
-    // tagItemList: tagItemReducer
-    tagItem
-
-})
+// const rootReducer = combineReducers({
+//     // tagItemList: tagItemReducer
+//     tagItemsListed
+//
+// })
 
 const constructStore = () => {
 
-    //debugo įjungimas
-    const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__ || compose;
+    return configureStore({
+        reducer: {
+            tagItemReducer
+        },
+        middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger)
+    })
 
-    return createStore(
-        rootReducer,
-        undefined,
-        composeEnhancer())
+
+
+    // //debugo įjungimas
+    // const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__ || compose;
+    //
+    // return createStore(
+    //     rootReducer,
+    //     undefined,
+    //     composeEnhancer())
 }
 
 export default constructStore
--- atmintine-ui/atmintine-ui/src/store/slices/tagItemSlice.js
+++ atmintine-ui/atmintine-ui/src/store/slices/tagItemSlice.js
@@ -1,34 +1,63 @@
 // store
-
 //Konstantos iškėlimas.
-const ADD_TO_LIST="ADD_TO_LIST"
-const REMOVE_FROM_LIST="REMOVE_FROM_LIST"
+import {createSlice} from "@reduxjs/toolkit";
+import {logger} from "redux-logger/src";
 
-//Reduceris
-const tagItemReducer = (state=[], action)=>{
+//
+// const ADD_TO_LIST = "ADD_TO_LIST"
+// const REMOVE_FROM_LIST = "REMOVE_FROM_LIST"
 
-    switch (action.type){
-        case ADD_TO_LIST:
-            return [...state, action.tagItem]
-        case REMOVE_FROM_LIST:
-            return state.filter((tagItem) => tagItem.id !== action.id)
-        default:
-            return state
+// 2.1 Redux toolkitas create slice.
+const tagItemSlice = createSlice({
+    name: 'tagItems',
+    initialState: [],
+    reducers: {
+
+        //    prisidedam funcijas.
+        addToTagItemList(state, {payload: tagItem}) {
+            state.push(tagItem)
+
+        },
+        removeFromItemList(state, {payload: tagItemID}) {
+            //filter yra gražiau atrodantis, tik filter retuną turi todėl ir reikia
+            // keisti sintaxe.
+            return state.filter((tagItem) => tagItem.id !== tagItemID)
+        }
+        // tagItem(state, action){
+        //     console.log(state)
+        //     console.log("Atėjo")
+        // }
     }
-
-}
-
-//Action creator
-const addToTagItemList = (tagItem) => ({
-        type: "ADD_TO_LIST",
-        tagItem
 })
 
-//Pridedu antrąjį reducerį.
-const removeFromItemList = (tagItemID) => ({
-    type: REMOVE_FROM_LIST,
-    tagItemID
-})
+// //Reduceris
+// const tagItemReducer = (state = [], action) => {
+//
+//     switch (action.type) {
+//         case ADD_TO_LIST:
+//             return [...state, action.tagItem]
+//         case REMOVE_FROM_LIST:
+//             return state.filter((tagItem) => tagItem.id !== action.id)
+//         default:
+//             return state
+//     }
+//
+// }
+//
+// //Action creator
+// const addToTagItemList = (tagItem) => ({
+//     type: "ADD_TO_LIST",
+//     tagItem
+// })
+//
+// //Pridedu antrąjį reducerį.
+// const removeFromItemList = (tagItemID) => ({
+//     type: REMOVE_FROM_LIST,
+//     tagItemID
+// })
+//
+// export default tagItemReducer
+// export {addToTagItemList, removeFromItemList}
 
-export default tagItemReducer
-export {addToTagItemList, removeFromItemList}
+export default tagItemSlice.reducer
+export const {addToTagItemList, removeFromItemList} = tagItemSlice.actions // čia yra action creatoriai.
